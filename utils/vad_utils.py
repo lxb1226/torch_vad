46ef88da4a095edcdbb001a6581a22ead9850e65
lxb1226 <1944303766@qq.com>
2022-06-14 17:08:35 +0800

    添加了绘图的功能

--- utils/vad_utils.py
+++ utils/vad_utils.py
@@ -1,4 +1,99 @@
+from typing import List
+
 import numpy as np
+
+
+def frame2time(n: int, frame_size: float = 0.032, frame_shift: float = 0.008
+               ) -> float:
+    return n * frame_shift + frame_size / 2
+
+
+def prediction_to_vad_label(
+        prediction: List[float],
+        frame_size: float = 0.032,
+        frame_shift: float = 0.009,
+) -> List[List[float]]:
+    speech_frames = []
+    prev = 0
+
+    start, end = 0, 0
+    for i, pred in enumerate(prediction):
+
+        if prev != pred:
+            if prev == 0:
+                # 0 -> 1
+                start = i
+            else:
+                # 1 -> 0
+                end = i
+                speech_frames.append(
+                    [frame2time(start, frame_size, frame_shift), frame2time(end, frame_size, frame_shift)])
+        elif i == len(prediction) - 1:
+            end = i
+            speech_frames.append(
+                [frame2time(start, frame_size, frame_shift), frame2time(end, frame_size, frame_shift)])
+        prev = pred
+
+    return speech_frames
+
+
+# def prediction_to_vad_label(
+#         prediction: List[float],
+#         frame_size: float = 0.032,
+#         frame_shift: float = 0.008,
+# ):
+#     """Convert model prediction to VAD labels.
+#
+#     Args:
+#         prediction (List[float]): predicted speech activity of each **frame** in one sample
+#             e.g. [0.01, 0.03, 0.48, 0.66, 0.89, 0.87, ..., 0.72, 0.55, 0.20, 0.18, 0.07]
+#         frame_size (float): frame size (in seconds) that is used when
+#                             extracting spectral features
+#         frame_shift (float): frame shift / hop length (in seconds) that
+#                             is used when extracting spectral features
+#         threshold (float): prediction values that are higher than `threshold` are set to 1,
+#                             and those lower than or equal to `threshold` are set to 0
+#     Returns:
+#         vad_label (str): converted VAD label
+#             e.g. "0.31,2.56 2.6,3.89 4.62,7.99 8.85,11.06"
+#
+#     NOTE: Each frame is converted to the timestamp according to its center time point.
+#     Thus the converted labels may not exactly coincide with the original VAD label, depending
+#     on the specified `frame_size` and `frame_shift`.
+#     See the following example for more detailed explanation.
+#
+#     Examples:
+#         >>> label = parse_vad_label("0.31,0.52 0.75,0.92")
+#         >>> prediction_to_vad_label(label)
+#         '0.31,0.53 0.75,0.92'
+#     """
+#     frame2time = lambda n: n * frame_shift + frame_size / 2
+#     speech_frames = {"speech_times": []}
+#
+#     prev_state = False
+#     start, end = 0, 0
+#     end_prediction = len(prediction) - 1
+#     for i, pred in enumerate(prediction):
+#         state = pred > threshold
+#         if not prev_state and state:
+#             # 0 -> 1
+#             start = i
+#         elif not state and prev_state:
+#             # 1 -> 0
+#             end = i
+#             speech_frames["speech_times"].append({"start_time": frame2time(start), "end_time": frame2time(end)})
+#             # speech_frames.append(
+#             #     "{:.3f},{:.3f}".format(frame2time(start), frame2time(end))
+#             # )
+#         elif i == end_prediction and state:
+#             # 1 -> 1 (end)
+#             end = i
+#             speech_frames["speech_times"].append({"start_time": frame2time(start), "end_time": frame2time(end)})
+#             # speech_frames.append(
+#             #     "{:.3f},{:.3f}".format(frame2time(start), frame2time(end))
+#             # )
+#         prev_state = state
+#     return speech_frames
 
 
 def find_contiguous_regions(activity_array):
@@ -28,3 +123,9 @@ def find_contiguous_regions(activity_array):
 
     # Reshape the result into two columns
     return change_indices.reshape((-1, 2))
+
+
+if __name__ == '__main__':
+    preds = [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1]
+    res = prediction_to_vad_label(preds)
+    print(res)
--- utils/viz.py
+++ utils/viz.py
@@ -1,10 +1,31 @@
-# import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import librosa
 
 
-# def label2rgb(label_np):
-#     label_color = np.argmax(label_np, axis=0)
-#     label_color = label_color / np.max(label_color) * 255
-#     label_color = cv2.applyColorMap(label_color.astype(np.uint8), 'jet')
-#     return label_color
+def plot_wave_and_label(data, speech_times, sr):
+    plt.figure(figsize=(15, 10))
+    sns.set()
+    sns.lineplot(x=[i / sr for i in range(len(data))], y=data)
 
+    start, end = 0, 0
+    for time in speech_times:
+        plt.axvspan(end, time[0], alpha=0.5, color="r")
+        start, end = time[0], time[1]
+        plt.axvspan(start, end, alpha=0.5, color="g")
+    plt.axvspan(end, (len(data) - 1) / sr, alpha=0.5, color="r")
+
+    plt.title(f"Sample number  with speech in green", size=20)
+    plt.xlabel("Time (s)", size=20)
+    plt.ylabel("Amplitude", size=20)
+    plt.xticks(size=15)
+    plt.yticks(size=15)
+    plt.show()
+
+
+if __name__ == '__main__':
+    audio_path = r'E:\workspace\GHT\vad\data\data\dataset\val\6411-58876-0056.wav'
+    speech_times = [[0.14, 3.76], [3.98, 5.80], [6.36, 11.06], [11.72, 12.82], [12.96, 15.36]]
+    wav_data, sr = librosa.load(audio_path, sr=8000)
+    plot_wave_and_label(wav_data, speech_times, sr)
